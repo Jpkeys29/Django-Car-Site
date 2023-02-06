488b66dba54e2c17d3b62f1a37694aaee688eada
Jpkeys29 <jpmusik29@gmail.com>
2023-02-06 17:25:34 -0500

    migrations

--- mycar_site/cars/models.py
+++ mycar_site/cars/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Car(models.Model):
+    #Car class is going to have an automated created pk 
     brand = models.CharField(max_length=30)
     year = models.IntegerField()
 
     #String representation
-
     def __str__(self):
         return f"The car is a {self.brand} {self.year}"
