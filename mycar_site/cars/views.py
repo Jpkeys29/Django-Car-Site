8cd5d16825694263221a1979e9b00434f62a4a90
Jpkeys29 <jpmusik29@gmail.com>
2023-02-07 15:31:00 -0500

    create car

--- mycar_site/cars/views.py
+++ mycar_site/cars/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
 from . import models 
 
 
@@ -12,7 +13,21 @@ def list(request):
 
 
 def add(request):
-    return render(request,'cars/add.html')
+    #To check what it is being send on the form
+    print(request.POST)
+    #If statement for when the form is sent:
+    if request.POST:
+        brand = request.POST['brand']
+        year = int(request.POST['year'])
+    #To create a new object:
+        models.Car.objects.create(brand=brand, year=year)
+    # if user submitted new car then go to list.html:
+        return redirect(reverse('cars:list'))
+    #initial return: return render(request,'cars/add.html')
+    else: #if nothing has been POST
+        return render(request,'cars/add.html')
+
+
 
 def delete(request):
     return render(request,'cars/delete.html')
